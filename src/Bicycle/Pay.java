3dcdc44ec9311c538148cc836c9854a248127cea
Drownfe <palaciojuanfe@gmail.com>
2022-07-20 22:05:26 -0500

    Pay bugs and organize

--- src/Bicycle/Pay.java
+++ src/Bicycle/Pay.java
@@ -9,11 +9,25 @@ import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Pay {
-    static Scanner sc = new Scanner(System.in);
     static int code;
     static int i;
     static double amount;
     static String status;
+    static Scanner sc = new Scanner(System.in);
+    public static void payTicket(){
+        System.out.println("Please enter ticket ID: ");
+        int code = sc.nextInt();
+        if (codes.contains(code)) {
+            loadTicket();
+            String user =userIDList.get(i);
+            String fullname = fullnameList.get(i);
+            code = code-1;
+            TicketHistory.saveTicket(code,user,fullname,amount,status);
+        } else {
+            System.out.println("Ticket not found, please try again");
+            boolean menu = true;
+        }
+    }
     public static void loadTicket() {
         for (int j =0; j < codes.size(); j++) {
             int register = codes.get(j);
@@ -31,36 +45,23 @@ public class Pay {
         amount = amountList.get(i);
         status = statusList.get(i);
         updateTicket(code, bicycle, user, date, starTime, endTime, helmet, condition, status, amount);
-        System.out.println("Amount to pay: " + amount + " usd.");
-        System.out.println("Please, insert the quantity of usd to pay: ");
+        System.out.println("Amount to pay: $" + amount + " USD");
+        System.out.println("Please, insert the quantity of USD to pay: ");
         double usd = sc.nextDouble();
         if (usd == amount) {
-            System.out.println("Thanks for paying. See you soon");
+            System.out.println("Thanks for paying. Come back soon!!!");
             status = "OK";
             updateTicket(code, bicycle, user, date, starTime, endTime, helmet, condition, status, amount);
         } else if (usd > amount) {
-            System.out.println("Thanks for paying. See you soon");
+            System.out.println("Thanks for paying. Come back soon!!!");
             double spare = usd - amount;
-            System.out.println("You have " + spare + " usd to spare.");
+            System.out.println("You have $" + spare + " USD to spare.");
             status = "OK";
             updateTicket(code, bicycle, user, date, starTime, endTime, helmet, condition, status, amount);
         }
         else if(usd<amount){
-            System.out.println("Please, insert the correct amount; You must to pay: " + amount + " usd");
+            System.out.println("Incorrect amount, You must pay: $" + amount + " USD");
         }
     }
-    public static void payTicket(){
-        System.out.println("Enter ID ticket: ");
-        int code = sc.nextInt();
-        if (codes.contains(code)) {
-            loadTicket();
-            String user =userIDList.get(i);
-            String fullname = fullnameList.get(i);
-            code = code-1;
-            TicketHistory.saveTicket(code,user,fullname,amount,status);
-        } else {
-            System.out.println("Ticket not found, please try again");
-            boolean menu = true;
-        }
-    }
+
 }
